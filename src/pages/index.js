8507afdafd2fb2f80a26e77d271fb5b34d119cb2
marian brinza <branza.marian03@yahoo.com>
2020-04-24 19:02:42 +0300

    get posts using graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,64 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import { graphql, StaticQuery } from 'gatsby';
+import Post from '../components/post';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home"/>
     <h1>Home page</h1>
+
+    <StaticQuery
+      query={indexQuery}
+      render={(data) => {
+        return (
+          <div>
+            {
+              data.allMarkdownRemark.edges.map(({ node }, index) => {
+                const postData = node.frontmatter;
+                return (
+                  <Post
+                    title={postData.title}
+                    date={postData.date}
+                    author={postData.author}
+                    path={postData.path}
+                    key={index}
+                  />
+                );
+              })
+            }
+          </div>
+        );
+      }}
+    />
+
   </Layout>
 );
+
+const indexQuery = graphql`
+  query query1 {
+    allMarkdownRemark(
+      sort: {
+      fields: frontmatter___date,
+      order: DESC
+      }
+    ){
+      edges {
+        node {
+          frontmatter {
+            title
+            author
+            date
+            path
+          }
+          id
+          excerpt
+        }
+      }
+    }
+  }
+`;
+
 
 export default IndexPage;
