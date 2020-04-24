3ffe30b93993f672ad7273fd4f06378bef92f9dc
marian brinza <branza.marian03@yahoo.com>
2020-04-24 14:15:02 +0300

    add about, tags, team pages

---
+++ src/pages/team.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const TeamPage = () => (
+  <Layout>
+    <SEO title="team"/>
+    <h1>team page</h1>
+  </Layout>
+);
+
+export default TeamPage;
