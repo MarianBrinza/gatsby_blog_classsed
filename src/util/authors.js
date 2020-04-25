1bc48b805b43667e5aba75ee45cbcd0f1daa31c8
marian brinza <branza.marian03@yahoo.com>
2020-04-25 22:58:53 +0300

    create authors array - mock db

---
+++ src/util/authors.js
@@ -0,0 +1,20 @@
+const authors = [
+  {
+    name: 'Jhon Doe',
+    imageUrl: 'man.jpg',
+    bio: 'I like stuff',
+    facebook: 'http://www.somefakeaddress.com',
+    twitter: 'http://www.somefakeaddress.com',
+    google: 'http://www.somefakeaddress.com'
+  },
+  {
+    name: 'Jane Doe',
+    imageUrl: 'woman.jpg',
+    bio: 'I like beautifull stuff',
+    facebook: 'http://www.somefakeaddress.com',
+    twitter: 'http://www.somefakeaddress.com',
+    google: 'http://www.somefakeaddress.com'
+  }
+];
+
+module.exports = authors;
