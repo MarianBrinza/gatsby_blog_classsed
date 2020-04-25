3c1f5ad3f7aefada75e5978bf022ce026c8179d9
marian brinza <branza.marian03@yahoo.com>
2020-04-25 19:25:45 +0300

    create slug

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,19 @@
  */
 
 // You can delete this file if you're not using it
+
+const { slugify } = require('./src/util/utility');
+
+exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const slugFromTitle = slugify(node.frontmatter.title);
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slugFromTitle
+    });
+  }
+
+};
