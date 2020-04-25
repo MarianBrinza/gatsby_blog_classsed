9c05ba9daccaf7988685141f6e5d87e51fa6b229
marian brinza <branza.marian03@yahoo.com>
2020-04-25 15:36:56 +0300

    add footer component

---
+++ src/components/footer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Footer = () => {
+
+  return (
+    <div className="site-footer">
+      <h4 className="text-center">
+        Some Blog
+      </h4>
+
+      <p className='text-center'>Follow you to Neverland</p>
+
+      <div className='footer-social-links'>
+        <ul className="social-links-list">
+          <li>
+            <a href="#" target='_blank' rel='noopener noreferrer' className='facebook'>
+              facebook icon here
+            </a>
+          </li>
+          <li>
+            <a href="#" target='_blank' rel='noopener noreferrer' className='twitter'>
+              twitter icon here
+            </a>
+          </li>
+          <li>
+            <a href="#" target='_blank' rel='noopener noreferrer' className='google'>
+              google icon here
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
