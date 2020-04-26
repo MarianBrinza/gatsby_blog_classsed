be22d13ee56c2195de34ad814cfec2e03eca7d3c
marian brinza <branza.marian03@yahoo.com>
2020-04-26 20:25:07 +0300

    tags page template

---
+++ src/templates/tags-page.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import { Badge, Button } from 'reactstrap';
+import { slugify } from '../util/utility';
+
+const TagsPage = ({ pageContext }) => {
+  const { tags, tagPostCounts } = pageContext;
+  return (
+    <Layout pageTitle='App Tags'>
+      <SEO title='All tags' keywords={['tags', 'topics']}/>
+      <ul>
+        {
+          tags.map((tag, index) => {
+            return (
+              <li key={index}>
+                <Button color='primary' href={`/tag/${slugify(tag)}`} style={{marginBottom: '10px'}}>
+                  {tag} <Badge color='light'>{tagPostCounts[tag]}</Badge>
+                </Button>
+              </li>
+            );
+          })
+        }
+      </ul>
+    </Layout>
+  );
+};
+
+export default TagsPage;
